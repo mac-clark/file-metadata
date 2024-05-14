5596abc850fa6194f569cfd67c5b15620b7916f6
MacKenzie Clark <clark.mac99@gmail.com>
2024-05-14 16:41:51 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +8,31 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    const { originalname, mimetype, size } = req.file;
+
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
 
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('App is listening on port ' + port)
 });
